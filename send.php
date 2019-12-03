34589342797e4fdbe28948e49b0d4337aca1ed9a
Michael Hernandez <michaelhern@hotmail.com>
2019-12-03 01:11:11 -0800

    Update send.php

--- send.php
+++ send.php
@@ -1,15 +1,54 @@
-Thank you for your message!
-
 <?php
-	
-	$userName 		= $_POST['name'];
-	$userEmail	 	= $_POST['email'];
-	$userMessage 		= $_POST['message'];
-	$to 			= "michaelhern@hotmail.com";
-	$subject 		= $_POST['category'];
-	$body 			= "Information Submitted:";
-	$body .= "\r\n Name: " . $userName;
-	$body .= "\r\n Email: " . $userEmail;
-	$body .= "\r\n Message: " . $userMessage;
-	mail($to, $subject, $body);
+ 
+if($_POST) {
+    $name = "";
+    $email = "";
+    $category = "";
+    $concerned_department = "";
+    $message = "";
+     
+    if(isset($_POST['name'])) {
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    }
+     
+    if(isset($_POST['email'])) {
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['$email']);
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+     
+    if(isset($_POST['category'])) {
+        $category = filter_var($_POST['category'], FILTER_SANITIZE_STRING);
+    }
+     
+    if(isset($_POST['message'])) {
+        $message = htmlspecialchars($_POST['message']);
+    }
+     
+    if($category == "Collaborative Opportunities") {
+        $recipient = "michaelhern@hotmail.com";
+    }
+    else if($category == "Employment") {
+        $recipient = "michaelhern@hotmail.com";
+    }
+    else if($category == "Networking") {
+        $recipient = "michaelhern@hotmail.com";
+    }
+    else {
+        $recipient = "michaelhern@hotmail.com";
+    }
+     
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $email . "\r\n";
+     
+    if(mail($recipient, $category, $message, $headers)) {
+        echo "<p>Thank you for your message!</p>";
+    } else {
+        echo '<p>Sorry, but your email did not go through.</p>';
+    }
+     
+} else {
+    echo '<p>Something went wrong</p>';
+}
+ 
 ?>
